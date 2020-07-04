b02177d604d55fde196dcd3d22ff0315e91831ee
Matheus Gonzatti <matheus.gonzatti@gmail.com>
2020-07-04 16:19:16 -0300

    Chore: Creating repository features.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: title,
+      url: url,
+      techs: [techs.split(',')]
+    })
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+
+    if(response.status === 204){ 
+      const newRepositories = repositories.filter(repo => repo.id !== id);
+      setRepositories(newRepositories);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => 
+          <li key={repository.id}>{repository.title}<button key={repository.id} onClick={() => handleRemoveRepository(repository.id)}>Remover</button> </li>
+        )}
       </ul>
 
+      <br />
+      Title: <input type="text" id="inputTitle" onChange={event => setTitle(event.target.value)} placeholder="Title of the repository."></input><br />
+      URL: <input type="text" id="inputUrl" onChange={event => setUrl(event.target.value)} placeholder="URL of the repository."></input><br />
+      Techs: <input type="text" id="inpuTechs" onChange={event => setTechs(event.target.value)} placeholder="Split the techs with comma."></input><br />
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
